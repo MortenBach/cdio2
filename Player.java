c5171f46dddd2bbf3b5e50e4b042c37092edec84
MortenBach <Mortenbnielsen@gmail.com>
2016-10-29 19:00:11 +0200

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,23 @@
+
+public class Player {
+
+			
+			private String name;
+			private int sum;
+			
+			
+			public Player(String name) {
+				this.name = name;
+			}
+			
+			
+			public String toString() {
+				return name;
+			}
+			
+			public String getName() {
+				return this.name;
+			}
+	}
+
+
