fe1f0c3945b6c3ecbf1d780d97e21a492f39b188
Evilwerner <Kristopuls@gmail.com>
2016-11-03 12:07:33 +0100

    Update TestRolls.java

--- TestRolls.java
+++ TestRolls.java
@@ -7,39 +7,35 @@ import org.junit.Test;
 
 public class TestRolls {
 	
-int numberOfRolls = 100000, result; //Thomasdad
+int numberOfRolls = 100000, result; //Number of repetitions.
 	
 	@Test
 	public void DiceIntervalTest() {
 		
-		Dice a = new Dice(); 
+		Dice a = new Dice(); //creates a reference to an instance of the object Dice.
 		
-		for (int roll = 0; roll <= numberOfRolls;)
+		for (int roll = 0; roll <= numberOfRolls;) //Loops untill i is equal to numberOfRolls
 		{
 			roll += 1;
 			result = a.roll();
-			
-			if ( result > 12 || result < 2)
-				break;
+			assertTrue(result >= 1 && result <= 6);  //tests if the amount of result is a number between and including 1 and 6.
 			
 		}
-		assertTrue(result >= 1 && result <= 6);
 	}
 
-	int dice2, dice3, dice4, dice5, dice6, dice7, dice8, dice9, dice10, dice11, dice12, counter;
-	
+	int dice2, dice3, dice4, dice5, dice6, dice7, dice8, dice9, dice10, dice11, dice12, counter; //Creates variables for the different outcome of the dice.
 	
 	@Test
 	public void valueOccur() {
 				
-		Dice b = new Dice();
-		Dice c = new Dice();
+		Dice b = new Dice(); //Makes a reference b to an instance of the object Dice.
+		Dice c = new Dice(); //Makes a reference c to an instance of the object Dice.
 		
-		for (int i = 1; i < numberOfRolls; i++)	{
+		for (int i = 1; i < numberOfRolls; i++)	{ // Loops till i is equal to numberOfRolls.
 			
-			counter = b.roll() + c.roll();
+			counter = b.roll() + c.roll(); //The variable counter is the first roll added to the second roll. 
 			
-			switch (counter){
+			switch (counter){ //counts every outcome and increment the variable in every instance of a case.
 			
 			case 2:
 				dice2 += 1;
@@ -79,7 +75,7 @@ int numberOfRolls = 100000, result; //Thomasdad
 		System.out.print("Number of 2`s: " + dice2 + "\tNumber of 3`s: " + dice3 + "\tNumber of 4`s: " + dice4 + "\tNumber of 5`s: " + dice5 + "\tNumber of 6`s: " + dice6 + "\tNumber of 7`s: " + dice7 + "\n \tNumber of 8`s: " + dice8 + "\tNumber of 9`s: " + dice9 + "\tNumber of 10`s: " + dice10 + "\tNumber of 11`s: " + dice11 + "\tNumber of 12`s: " + dice12);
 		
 		
-		assertTrue(dice2 >= (2780-500) && dice2 <= (2780+500));
+		assertTrue(dice2 >= (2780-500) && dice2 <= (2780+500)); //tests if the number of outcomes is close to the statistic amount of outcomes there should be. 
 		assertTrue(dice3 >= (5560-500) && dice3 <= (5560+500));
 		assertTrue(dice4 >= (8330-500) && dice4 <= (8330+500));
 		assertTrue(dice5 >= (11110-500) && dice5 <= (11110+500));
