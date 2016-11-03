742c7e777414e2b01e83b475030baf5c439f5262
Stuart86 <stuart_mclean86@hotmail.com>
2016-11-03 09:48:26 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
-public class Main {
 
-	public static void main(String[] args) {
+public class Main 
+{
+	public static void main(String[] args) 
+	{
 	
 		Controller AdventureGame = new Controller();
 		AdventureGame.game();
-
 	}
-
 }
