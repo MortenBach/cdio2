23cecdd21e8b6e354c4ebc66c4be8f7234e1e19e
Evilwerner <Kristopuls@gmail.com>
2016-11-03 10:45:55 +0100

    Update testNegative.java

--- testNegative.java
+++ testNegative.java
@@ -1,5 +1,33 @@
-package lektion2;
+import static org.junit.Assert.*;
 
-public class testNegative {
+import org.junit.Test;
+
+public class TestNegative {
+	private Player testPlayer;
+	private int field1;
+	public TestNegative() {
+		testPlayer = new Player("testPlayer");
+	}
+
+	@Test
+	public void testgame() {
+
+		for (int i = 0; i < 20; i++) {
+			field1 = (int) ((Math.random()*(-10000))-1);
+			testPlayer = testPlayer.getPlayer();
+			System.out.println("Attempted to withdraw " + field1 + "credit.");
+			testPlayer.setPoints(field1);
+			avoidNegative();
+			System.out.println(testPlayer + " now has " + testPlayer.getPoints() + " credit. ");
+			assertTrue(testPlayer.getPoints() >= 0);
+
+		}
+
+	}
+	public void avoidNegative() {
+		if (testPlayer.getPoints() <= 0)
+			testPlayer.setZero();
+
+	}
 
 }
