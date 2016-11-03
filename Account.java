93549b14c67a680d52323e290c5fcfc2b7c9431a
Stuart86 <stuart_mclean86@hotmail.com>
2016-11-03 09:42:36 +0100

    Update Account.java

--- Account.java
+++ Account.java
@@ -3,18 +3,21 @@ public class Account {
 	
 	private int points = 1000;
 	
-	public Account() {
+	public Account() 
+	{
 	}
-
-	public int getPoints() {
+	public int getPoints() 
+	{
 		return points;
 	}
-
-	public void setPoints(int points) {
+	public void setPoints(int points) 
+	{
 		
 		this.points += points;
 	}
-	
-	
-	
+	public void setZero() 
+	{
+		
+		points = 0;
+	}
 }
